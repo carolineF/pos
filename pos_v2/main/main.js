e767903f9adde95c856c9ae27eb4d21c83b66e31
carolineF <1138483832@qq.com>
2015-07-23 15:35:28 +0800

    add script 标签

--- pos_v2/main/main.js
+++ pos_v2/main/main.js
@@ -1,11 +1,4 @@
 function printReceipt(tags) {
 
-  var cartItems = [];
 
-  tags.forEach(function(tag){
-    cartItems = new CartItem().createCartItems(cartItems, tag);
-  });
-
-  var receipt = new Receipt().createReceipt(cartItems);
-  console.log(receipt);
 }
